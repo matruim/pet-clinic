458d058d3cfc30b040576db13fc94c7dac39a8e9
Jared Good <jared_danell@yahoo.com>
2022-06-28 11:36:03 -0600

    Implement Visit Map Service. Closes #45

---
+++ pet-clinic-data/src/main/java/com/jaredgood/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.jaredgood.petclinic.services;
+
+import com.jaredgood.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
--- pet-clinic-data/src/main/java/com/jaredgood/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/jaredgood/petclinic/services/map/VisitMapService.java
@@ -1,4 +1,36 @@
 package com.jaredgood.petclinic.services.map;
 
-public class VisitMapService {
+import com.jaredgood.petclinic.model.Visit;
+import com.jaredgood.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
 }
