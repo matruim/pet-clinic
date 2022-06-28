636402fc49b056e26b4ee7657e9987e3bcb21a78
Jared Good <jared_danell@yahoo.com>
2022-06-28 10:14:25 -0600

    Setup Entity for Owner, Person, Pet, and PetType. Closes #35

--- pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Owner.java
@@ -1,12 +1,19 @@
 package com.jaredgood.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
+    @Column
     private String address;
+    @Column
     private String city;
+    @Column
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
