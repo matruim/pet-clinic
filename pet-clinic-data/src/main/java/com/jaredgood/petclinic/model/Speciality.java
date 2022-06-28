34a1041984d1981810153ee5a6b9539c50520a50
Jared Good <jared_danell@yahoo.com>
2022-06-28 10:31:52 -0600

    Setup Entity for Vet and Specialties. Closes #36

--- pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package com.jaredgood.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
+    @Column
     private String description;
 
     public String getDescription() {
