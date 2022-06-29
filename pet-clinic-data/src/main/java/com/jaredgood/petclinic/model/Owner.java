8e65f3ecf096075b56a3cb7e520b5363c7f156c3
Jared Good <jared_danell@yahoo.com>
2022-06-29 08:52:59 -0600

    refactor for project lombok. Closes # 51

--- pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Owner.java
@@ -1,9 +1,15 @@
 package com.jaredgood.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
@@ -16,35 +22,12 @@ public class Owner extends Person{
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+    @Builder
+    public Owner(Long Id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(Id, firstName, lastName);
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
         this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
 }
--- pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jaredgood/petclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package com.jaredgood.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -11,11 +17,9 @@ public class Vet extends Person{
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
+    @Builder
+    public Vet(Long id, String firstName, String lastName, Set<Speciality> specialities) {
+        super(id, firstName, lastName);
         this.specialities = specialities;
     }
 }
