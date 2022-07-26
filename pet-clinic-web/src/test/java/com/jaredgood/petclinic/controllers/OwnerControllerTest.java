99b295d01238ae377bc27e27dc3e961a65f62a20
Jared Good <jared_danell@yahoo.com>
2022-07-26 09:53:17 -0600

    fix issue with Owner Display. Closes #62

--- pet-clinic-web/src/test/java/com/jaredgood/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/jaredgood/petclinic/controllers/OwnerControllerTest.java
@@ -15,7 +15,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -81,5 +82,14 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().Id(1L).build());
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().is(200))
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
